2b22ee10074625623907ff2612c44c32cb02990f
shdopeoplesn <shdopeoplesn@gmail.com>
2019-12-16 10:19:10 +0800

    Change store data type from list to dictionaries

--- srv_socket.py
+++ srv_socket.py
@@ -17,19 +17,17 @@ class Device():
 
 
 global g_devices
-g_devices = []
+g_devices = {}
 
 #Remove Device data from global list g_devices
 def RemoveDeviceById(id):
-    for element in g_devices:
-        if element.id_ == id:
-            g_devices.remove(element)
+    del g_devices[id]
 
 # Called for every client connecting (after handshake)
 def NewClient(client, server):
     print("New client connected and was given id %d" % client['id'])
     #store client's data
-    g_devices.append(Device(client['id']))
+    g_devices[client['id']] = Device(client['id'])
 
 
 # Called for every client disconnecting
@@ -41,7 +39,8 @@ def ClientLeft(client, server):
 def MessageReceived(client, server, message):
     message = base64.b64decode(message).decode('UTF-8','strict')
     print("Client(%d) said: %s" % (client['id'], message))
-    server.send_message_to_all("Client(%d) said: %s" % (client['id'], message))
+
+    #server.send_message_to_all("Client(%d) said: %s" % (client['id'], message))
     
 def SocketServerStart():
     server = WebsocketServer(SOCKET_PORT, HOST)
