0f575c56771cf8cb1200d98f8b30e12d73486b83
kidian <shdopeoplesn@gmail.com>
2020-01-21 09:02:19 +0800

    add logs function

--- lib_logs.py
+++ lib_logs.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python
 from datetime import datetime
+import threading
+import time
+import logging
 
 def PrintMsg(msg):
     now = datetime.now() # current date and time
+    #write to log file
+    date = now.strftime("%m-%d-%Y")
+    logging.basicConfig(filename='logs/' + date + '.log',level=logging.DEBUG,format='%(asctime)s %(message)s',datefmt='%m/%d/%Y %I:%M:%S %p')
+    logging.info(msg)
+
+    #show msg to console
     date_time = now.strftime("%Y/%m/%d %H:%M:%S")
     print('[' + date_time + '] ' + msg)
+    
